b69a467c2628c5274f8c9206d3b4ccbd6139307f
Victor Olateju <victor.olateju@matblas.com>
2024-12-11 08:04:27 +0000

    allowed hosts

--- server/settings.py
+++ server/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-co^+5fjilzhx16kl7d=tq0#91ir-t%byl*13%y^!$)qj$vvuk+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['.vercel.app', 'localhost']
